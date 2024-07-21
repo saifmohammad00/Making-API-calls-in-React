a410879f26ff3de767def7fe5f541e6b87fe88c8
saifmohammad00 <saifmohammad592001@gmail.com>
2024-07-21 19:11:32 +0530

    Storing and deleting the new movies into the Databases

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
@@ -6,87 +6,107 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [formData,setFormData]=useState({
-    title:"",
-    openingText:"",
-    releaseDate:""
-  });
 
-  const fetchMoviesHandler=useCallback(async ()=>{
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-    console.log("hello")
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-movies-app-44446-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json');
       if (!response.ok) {
         throw new Error("something went wrong");
       }
       const data = await response.json();
-
-      const transformedMovies = data.results.map((item) => {
-        return {
-          id: item.episode_id,
-          title: item.title,
-          openingText: item.opening_crawl,
-          releaseDate: item.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
+      setMovies(loadedMovies);
     }
     catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-  },[])
-  useEffect(()=>{
+  }, [])
+  useEffect(() => {
     fetchMoviesHandler();
-  },[fetchMoviesHandler]);
-
+  }, [fetchMoviesHandler]);
+  const deleteHandler = async (id) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(`https://react-movies-app-44446-default-rtdb.asia-southeast1.firebasedatabase.app/movies/${id}.json`, {
+        method: 'DELETE',
+      });
+      if (!response.ok) {
+        throw new Error('Failed to delete movie.');
+      }
+      setMovies(prevMovies => prevMovies.filter(movie => movie.id !== id));
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  };
   let content = <p>Found No Movies.</p>;
   if (movies.length > 0) {
-    content = <MoviesList movies={movies} />
+    content = <MoviesList movies={movies} deleteHandler={deleteHandler}/>
   }
   if (error) {
-    content =<p>{error}</p>
+    content = <p>{error}</p>
   }
   if (isLoading) {
     content = <p>Loading...</p>
   }
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
-  const consoleBrowser = (event) => {
+  const titleref = useRef(null);
+  const openingref = useRef(null);
+  const releaseref = useRef(null);
+
+  async function formHandler(event) {
     event.preventDefault();
-    console.log(formData);
-    setFormData({
-      title: '',
-      openingText: '',
-      releaseDate: '',
-    });
+    if (titleref.current.value === "" || openingref.current.value === "" || releaseref.current.value === "") {
+      return;
+    }
+    const newMovie = {
+      title: titleref.current.value,
+      openingText: openingref.current.value,
+      releaseDate: releaseref.current.value
+    }
+    titleref.current.value = '';
+    openingref.current.value = '';
+    releaseref.current.value = '';
+    const response = await fetch('https://react-movies-app-44446-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json', {
+      method: "POST",
+      body: JSON.stringify(newMovie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json();
+    console.log(data);
   };
   return (
     <React.Fragment>
       <section>
         <form>
-          <div style={{textAlign:"left"}}>
-            <strong>Title</strong><br/>
-            <input type='text' name="title" value={formData.title} onChange={handleChange}/>
+          <div style={{ textAlign: "left" }}>
+            <strong>Title</strong><br />
+            <input type='text' name="title" ref={titleref} />
           </div>
-          <br/>
-          <div style={{textAlign:"left"}}>
-            <strong>Opening Text</strong><br/>
-            <input type='text' name="openingText" value={formData.openingText} onChange={handleChange}/>
+          <br />
+          <div style={{ textAlign: "left" }}>
+            <strong>Opening Text</strong><br />
+            <input type='text' name="openingText" ref={openingref} />
           </div>
-          <br/>
-          <div style={{textAlign:"left"}}>
-            <strong>Release Date</strong><br/>
-            <input type='date' name="releaseDate" value={formData.releaseDate} onChange={handleChange}/>
+          <br />
+          <div style={{ textAlign: "left" }}>
+            <strong>Release Date</strong><br />
+            <input type='text' name="releaseDate" ref={releaseref} />
           </div>
-          <button onClick={consoleBrowser}>Add Movie</button>
+          <button onClick={formHandler}>Add Movie</button>
         </form>
       </section>
       <section>
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -4,14 +4,19 @@ import Movie from './Movie';
 import classes from './MoviesList.module.css';
 
 const MovieList = (props) => {
+  const handledelete=(id)=>{
+    props.deleteHandler(id);
+  }
   return (
     <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
         <Movie
           key={movie.id}
+          id={movie.id}
           title={movie.title}
           releaseDate={movie.releaseDate}
           openingText={movie.openingText}
+          onClick={handledelete}
         />
       ))}
     </ul>
